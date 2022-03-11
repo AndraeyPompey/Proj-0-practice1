ad933e2ae7c90e557b1d2495af3a0afc23de0544
AndraeyPompey <andraey.pompey@gmail.com>
2022-03-11 15:03:08 -0500

    created entities for project

---
+++ entities/account_class.py
@@ -0,0 +1,8 @@
+
+
+
+class Account:
+    def __init__(self, cust_id, account_id, balance):
+        self.cust_id = cust_id
+        self.account_id = account_id
+        self.balance = balance
---
+++ entities/customer_class.py
@@ -0,0 +1,11 @@
+
+
+
+class Customer:
+
+    def __init__(self, cust_id, first_name, last_name):
+        self.cust_id = cust_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+
